5456b0dedf2e64cad7b999150498f7041d19bfaa
Axmedd <107942966+Axmedd@users.noreply.github.com>
2022-08-10 08:24:55 +0100

    HTML and CSS with functionality

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const latestButton = document.getElementById("latest-btn");
+const sportsButton = document.getElementById("sports-btn");
+const covidButton = document.getElementById("covid-cases-btn");
+const pageContent = document.getElementById("page-content");
+const sportsSection = document.getElementById("sports-section");
+const latestSection = document.getElementById("latest-section");
+const covidSection = document.getElementById("covid-section");
+
+latestButton.addEventListener("click", openLatestPage);
+
+function openLatestPage() {
+  console.log("Latest Page Opened");
+  latestButton.classList.add.hidden;
+  pageContent.classList.remove("hide");
+  latestButton.remove();
+  sportsButton.remove();
+  covidButton.remove();
+  sportsSection.remove();
+}
+
+sportsButton.addEventListener("click", openSportPage);
+
+function openSportPage() {
+  console.log("Sport Page Opened");
+  latestButton.classList.add.hidden;
+  pageContent.classList.remove("hide");
+  latestButton.remove();
+  sportsButton.remove();
+  covidButton.remove();
+  latestSection.remove();
+}
+
+covidButton.addEventListener("click", openCovidPage);
+
+function openCovidPage() {
+  console.log("Covid Page Opened");
+  latestButton.classList.add.hidden;
+  pageContent.classList.remove("hide");
+  latestButton.remove();
+  sportsButton.remove();
+  covidButton.remove();
+  latestSection.remove();
+  sportsSection.remove();
+}
