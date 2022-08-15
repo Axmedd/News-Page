0f29e1c64ecff4c4e58f7bc4ea0b62749b1d5c65
Axmedd <107942966+Axmedd@users.noreply.github.com>
2022-08-15 20:18:03 +0100

    Finished Latest Headline With Link

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ const pageContent = document.getElementById("page-content");
 const sportsSection = document.getElementById("sports-section");
 const latestSection = document.getElementById("latest-section");
 const covidSection = document.getElementById("covid-section");
+const latestHeadlines = document.getElementById("latestheadlines");
+const latestLinks = document.getElementById("links");
+const headlineList = document.getElementById("headlineList");
 
 latestButton.addEventListener("click", openLatestPage);
 
@@ -44,19 +47,56 @@ function openCovidPage() {
 }
 
 // fetchButton.addEventListener("click", getApi);
+function handleNewsApi() {
+  let firstApiUrl =
+    "http://api.mediastack.com/v1/news?access_key=28c721d782d6d69f95ee0cc65221b176&date=2022-08-08&countries=gb";
 
-firstApiUrl =
-  "http://api.mediastack.com/v1/news?access_key=774d6825387c91706870d36a208f2f53&date=2022-08-08&countries=gb";
+  fetch(firstApiUrl)
+    .then((response) => response.json())
+    .then((firstApiData) => handleNewsApiData(firstApiData));
+
+  function handleNewsApiData(firstApiData) {
+    //console.log(firstApiData.data);
+    // headlineList = document.createElement("ul");
+    for (var i = 0; i < 25; i++) {
+      //create a html tag
+      //append text content
+      console.log(firstApiData.title);
+
+      if (firstApiData.data[i].category === "sports") {
+        handleSportsData(firstApiData.data[i]);
+      } else {
+        handleNewsData(firstApiData.data[i]);
+      }
+    }
+  }
+}
+
+function handleNewsData(newsData) {
+  console.log(newsData);
+  // h2ForLatest = document.createElement("h2");
+  // latestHeadlines.classList.add("is-four-fifths");
+  // h2ForLatest.textContent = newsData.title;
+  // latestHeadlines.appendChild(h2ForLatest);
+  listHead = document.createElement("li");
+  linkForLatest = document.createElement("a");
+  linkForLatest.classList.add("link-button");
+  // linkForLatest = document.createElement("");
+  linkForLatest.setAttribute("href", newsData.url);
+  console.log(newsData);
+  linkForLatest.textContent = newsData.title;
+  //latestHeadlines.appendChild(linkForLatest);
+  listHead.appendChild(linkForLatest);
+  headlineList.appendChild(listHead);
+}
+
+function handleSportsData(sportsData) {}
+//console.log(data[i].description);
 
 secondApiUrl =
   "https://api.covid19api.com/total/country/united-kingdom/status/confirmed?from=2022-03-01T00:00:00Z&to=2022-08-01T00:00:00Z";
-
-fetch(firstApiUrl)
-  .then((response) => response.json())
-  .then((firstApiData) => console.log(firstApiData));
-
-//console.log(data[i].description);
-
 fetch(secondApiUrl)
   .then((response) => response.json())
   .then((secondApiData) => console.log(secondApiData));
+
+handleNewsApi();
